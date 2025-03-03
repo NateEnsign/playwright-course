44d9d08f2764ac4e0355f8dfd6c4565a08bffdfe
Nate Ensign <dixienate@gmail.com>
2025-03-03 08:38:54 -0700

    added a base url and some of my first locators

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,2 +1,11 @@
 import {test} from '@playwright/test'
 
+test("Product Page Add To Basket", async ({page}) => {
+    await page.goto("localhost:2221")
+
+    const addToBasketButton = page.locator('[data-qa"product-button"]').first()
+    const basketCounter = page.locator('[data-qa="header-basket-count"]')
+    
+
+    await page.pause()
+})
