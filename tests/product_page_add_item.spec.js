d5d5bb877441c8d2a13d1302c5c703e832c83a1c
Nate Ensign <dixienate@gmail.com>
2025-03-03 08:53:10 -0700

    first test clicking the addToBasket button

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -3,9 +3,8 @@ import {test} from '@playwright/test'
 test("Product Page Add To Basket", async ({page}) => {
     await page.goto("localhost:2221")
 
-    const addToBasketButton = page.locator('[data-qa"product-button"]').first()
-    const basketCounter = page.locator('[data-qa="header-basket-count"]')
-    
+    const addToBasketButton = page.locator('div').filter({ hasText: /^499\$Add to Basket$/ }).getByRole('button')
+    await addToBasketButton.click()
 
     await page.pause()
 })
