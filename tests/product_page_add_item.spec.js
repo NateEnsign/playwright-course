8dc37df72b403806e26679490d26a082ffa9b63a
Nate Ensign <dixienate@gmail.com>
2025-03-03 09:02:23 -0700

    added a small check to give more clear error messages when something in test fails

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -4,6 +4,7 @@ test("Product Page Add To Basket", async ({page}) => {
     await page.goto("localhost:2221")
 
     const addToBasketButton = page.locator('div').filter({ hasText: /^499\$Add to Basket$/ }).getByRole('button')
+    await addToBasketButton.waitFor() //using .waitFor() gives a much more clear error message for why the test failed
     await addToBasketButton.click()
 
     await page.pause()
