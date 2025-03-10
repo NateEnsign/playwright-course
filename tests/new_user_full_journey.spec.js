66f9cea00d66384039793cd12c052b8daa48ff94
Nate Ensign <dixienate@gmail.com>
2025-03-10 14:38:06 -0600

    added new method to test adding product to basket

--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,8 +1,11 @@
-import {test} from '@playwright/test';
-import { ProductPage } from '../page-objects/ProductPage';
+import { test } from "@playwright/test";
+import { ProductPage } from "../page-objects/ProductPage";
 
-test.only('New user full end-to-end test journey', async ({ page }) => {
-    const productPage = new ProductPage(page);
-    await productPage.visit();
-    await page.pause();
-})
+test.only("New user full end-to-end test journey", async ({ page }) => {
+  const productPage = new ProductPage(page);
+  await productPage.visit();
+  await productPage.addProductToBasket(0);
+  await productPage.addProductToBasket(1);
+  await productPage.addProductToBasket(2);
+  await page.pause();
+});
