21afeb09adabe559a905134af0b8c4aa8fd0fa09
Nate Ensign <dixienate@gmail.com>
2025-03-10 12:20:29 -0600

    added test to verify checkout btn takes us to the basket page

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -5,6 +5,7 @@ test("Product Page Add To Basket", async ({ page }) => {
 
   const addToBasketButton = page.locator('[data-qa="product-button"]').first();
   const basketCounter = page.locator('[data-qa="header-basket-count"]');
+  
 
   await addToBasketButton.waitFor(); //using .waitFor() gives a much more clear error message for why the test failed
   await expect(addToBasketButton).toHaveText("Add to Basket");
@@ -14,6 +15,17 @@ test("Product Page Add To Basket", async ({ page }) => {
 
   await expect(addToBasketButton).toHaveText("Remove from Basket");
   await expect(basketCounter).toHaveText("1");
+
+  const checkoutLink = page.getByRole('link', { name: 'Checkout' });
+
+  await checkoutLink.waitFor();
+  await expect(checkoutLink).toHaveText("Checkout");
+  
+  await checkoutLink.click();
+
+  await page.waitForURL('/basket');
+
+
 
   await page.pause();
 });
