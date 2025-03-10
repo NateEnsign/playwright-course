7a99a6dcaacd0361fc33f9b2a527bee3206198ef
Nate Ensign <dixienate@gmail.com>
2025-03-10 15:36:36 -0600

    added new method to Navigation page object to test goToCheckout and implemented in test

--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import { ProductPage } from "../page-objects/ProductPage";
+import { Navigation } from '../page-objects/Navigation';
 
 test.only("New user full end-to-end test journey", async ({ page }) => {
   const productPage = new ProductPage(page);
@@ -7,5 +8,7 @@ test.only("New user full end-to-end test journey", async ({ page }) => {
   await productPage.addProductToBasket(0);
   await productPage.addProductToBasket(1);
   await productPage.addProductToBasket(2);
+  const navigation = new Navigation(page);
+  await navigation.goToCheckout();
   await page.pause();
 });
