5b2a53270d80a4ea07d392560d2412c108d8d6e0
Nate Ensign <dixienate@gmail.com>
2025-03-12 15:08:06 -0600

    completed method removeCheapestProduct so it now validates if the card count goes down by 1

--- page-objects/Checkout.js
+++ page-objects/Checkout.js
@@ -1,23 +1,29 @@
+import { expect } from '@playwright/test';
+
 export class Checkout {
-    constructor(page){
-        this.page = page;
+  constructor(page) {
+    this.page = page;
 
-        this.basketCards = page.locator('[data-qa="basket-card"]');
-        this.basketItemPrice = page.locator('[data-qa="basket-item-price"]');
-        this.basketItemRemoveButton = page.locator('[data-qa="basket-card-remove-item"]');
+    this.basketCards = page.locator('[data-qa="basket-card"]');
+    this.basketItemPrice = page.locator('[data-qa="basket-item-price"]');
+    this.basketItemRemoveButton = page.locator(
+      '[data-qa="basket-card-remove-item"]'
+    );
+  }
 
-    }
+  removeCheapestProduct = async () => {
+    await this.basketCards.first().waitFor();
+    const itemsBeforeRemoval = await this.basketCards.count();
+    await this.basketItemPrice.first().waitFor();
+    const allPrice = await this.basketItemPrice.allInnerTexts();
+    const numberPrices = allPrice.map((el) => +el.replace("$", ""));
+    const smallestPrice = Math.min(...numberPrices);
+    const smallestPriceInx = numberPrices.indexOf(smallestPrice);
+    const specificRemoveBtn = this.basketItemRemoveButton.nth(smallestPriceInx);
+    await specificRemoveBtn.waitFor();
+    await specificRemoveBtn.click();
+    await expect(this.basketCards).toHaveCount(itemsBeforeRemoval - 1);
 
-    removeCheapestProduct = async () => {
-        await this.basketCards.first().waitFor();
-        await this.basketItemPrice.first().waitFor();
-        const allPrice = await this.basketItemPrice.allInnerTexts();
-        const numberPrices = allPrice.map((el) => +el.replace('$',''));
-        const smallestPrice = Math.min(...numberPrices);
-        const smallestPriceInx = numberPrices.indexOf(smallestPrice);
-        await this.basketItemRemoveButton.nth(smallestPriceInx).waitFor()
-        await this.basketItemRemoveButton.nth(smallestPriceInx).click();
-
-        await this.page.pause()
-    }
+    await this.page.pause();
+  };
 }
